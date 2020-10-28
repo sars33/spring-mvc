38e453a7ecdb3a6e66bfa760d170bb12ef76aed1
sars33 <sergetester33@mail.com>
2020-10-28 23:30:55 +0200

    Java pre-project. Practical task 2.2.2, edited, removed unrequited imports

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -1,15 +1,12 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.UserService;
 import web.service.UserServiceImp;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
